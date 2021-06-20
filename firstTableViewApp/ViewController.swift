09b9306167fa73ef98e946dad982c8bef774c947
ziman <alex.cheranov@yandex.ru>
2021-06-20 11:34:41 +0300

    Initial Commit

---
+++ firstTableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstTableViewApp
+//
+//  Created by Алексей Черанёв on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
