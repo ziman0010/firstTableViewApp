7bcf1c988f82fe3e9e781227241ab093cef44096
ziman <alex.cheranov@yandex.ru>
2021-06-20 12:28:13 +0300

    Custom Cell

---
+++ firstTableViewApp/EmojiTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiTableViewCell.swift
+//  firstTableViewApp
+//
+//  Created by Алексей Черанёв on 20.06.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
